e2ee7001de2177ee3ee5db646f8db711fe29df1c
Nathaniel Honka <nhonka@apple.com>
2020-03-28 19:52:50 -0700

    working code

--- rrt_star.py
+++ rrt_star.py
@@ -1,5 +1,15 @@
 import numpy as np
 import random
+import math
+from matplotlib import pyplot as plt
+
+random.seed(1)
+
+
+def X_distance(position1, position2):
+  x1, y1 = position1
+  x2, y2 = position2
+  return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 
 class Node:
@@ -9,8 +19,8 @@ class Node:
     # Cost to get to this node from root.
     self.cost = cost
 
-  def distance_squared(self, other_position):
-    return (self.position[0] - other_position[0])**2 + (self.position[1] - other_position[1])**2
+  def __str__(self):
+    return f"parent: {self.parent}; cost: {self.cost}"
 
 
 class Tree:
@@ -20,37 +30,46 @@ class Tree:
     self.goal_node_idxs = []
 
   def add(self, node):
-    if len(self.nodes >= self.max_nodes):
+    if len(self.nodes) >= self.max_nodes:
       return None
 
     self.nodes.append(node)
     idx_added_node = len(self.nodes) - 1
 
-    if self.at_goal(node.position):
-     self.goal_node_idxs.append(idx_added_node)
-
     return idx_added_node
 
-  def nearest_idxs(self, position, radius=None):
+  def near_idxs(self, position, radius):
     near_nodes_idx = []
     near_nodes_ds = []
     for i, node in enumerate(self.nodes):
-      this_ds = node.distance_squared(position)
-      if this_ds < min_ds:
+      this_ds = X_distance(node.position, position)
+      if this_ds < radius:
         near_nodes_idx.append(i)
         near_nodes_ds.append(this_ds)
 
     # Return the near set.
     return near_nodes_idx
 
+  def nearest(self, position):
+    nearest_idx = None
+    nearest_dist = 10e9
+    for i, node in enumerate(self.nodes):
+      this_dist = X_distance(node.position, position)
+      if this_dist < nearest_dist:
+        nearest_idx = i
+        nearest_dist = this_dist
+
+    # Return the near set.
+    return nearest_idx
+
 
 class World:
   def __init__(self):
     self.xrange = [-10, 10]
     self.yrange = [-10, 10]
-    self.initial_position = (0,0)
+    self.initial_position = (0, 0)
     # Refers to Z dimension.
-    self.goal_pose_z = 5
+    self.goal_pose_z = 0
     # Distance at which to create new nodes from network.
     self.dq = 0.5
     # Precision of collision checking along paths between nodes.
@@ -58,12 +77,6 @@ class World:
 
     num_obstacles = 2
     self.obstacles = self.make_obstacles(self.xrange, self.yrange, num_obstacles)
-
-  @staticmethod
-  def distance(position1, position2):
-    x1, y1 = position1
-    x2, y2 = position2
-    return sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
   @staticmethod
   def make_obstacles(xrange, yrange, num_obstacles):
@@ -99,8 +112,11 @@ class World:
     return is_collision
 
   def has_collision(self, X0, X1):
+    if X0 == X1:
+      return self.is_collision(X0)
+
     # Number of intermediate points, including origin.
-    path_count = math.floor(self.distance(X0, X1) / self.precision)
+    path_count = math.ceil(X_distance(X0, X1) / self.precision)
     x0, y0 = X0
     x1, y1 = X1
     dx = x0 / path_count
@@ -110,27 +126,31 @@ class World:
       xi = x0 + i * dx
       yi = y0 + i * dy
       Xi = (xi, yi)
-      if is_collision(Xi):
+      if self.is_collision(Xi):
         return True
+    if self.is_collision(X1):
+      return True
     return False
 
   def at_goal(self, position):
-    if abs(z(position) - self.goal_pose_z) < self.dq:
+    z_resolution = 0.1
+    if abs(self.z(position) - self.goal_pose_z) < z_resolution:
       return True
     else:
       return False
 
   # In our state space, the robot has DOFs X, Y, and "pose" / output state z = f(X) = f(x,y)
   def z(self, position):
-    x, y = X
-    z = 2 * x - y
+    g_x, g_y = 4, 0
+    x, y = position
+    z = (x-g_x)**2 + (y-g_y)**2
     return z
 
   def random_X(self):
     return (random.uniform(*self.xrange), random.uniform(*self.yrange))
 
   def steer(self, root_position, goal_position):
-    d = self.distance(root_position, goal_position)
+    d = X_distance(root_position, goal_position)
     dt = self.dq / d  # parametric distance from root to goal
 
     x_root, y_root = root_position
@@ -141,13 +161,17 @@ class World:
 
     x_out = x_root + dx
     y_out = y_root + dy
-    return x_root, y_root
+    return x_out, y_out
 
   def rrt_star(self):
     root = Node(self.initial_position, None, 0)
     tree = Tree(root)
 
-    max_expansion = 10
+    # TODO check if root at goal already.
+
+    fig, ax = plt.subplots()
+
+    max_expansion = 100
     for i in range(max_expansion):
       # TODO Add occasional greedy choice.
       X_random = self.random_X()
@@ -165,32 +189,44 @@ class World:
         n_nearest = tree.nodes[nearest_node_idx]
         best_parent_idx = nearest_node_idx
         # Minimum cost to get to X_new through neighbors.
-        cost_through_best_parent = n_nearest.cost + self.distance(n_nearest.position, X_new)
+        cost_through_best_parent = n_nearest.cost + X_distance(n_nearest.position, X_new)
         for neighbor_idx in neighbor_idxs:
           n_neighbor = tree.nodes[neighbor_idx]
-          new_cost_through_neighbor = n_neighbor.cost + self.distance(n_neighbor.position, X_new)
+          new_cost_through_neighbor = n_neighbor.cost + X_distance(n_neighbor.position, X_new)
           if new_cost_through_neighbor < cost_through_best_parent and not self.has_collision(
                   n_neighbor.position, X_new):
             best_parent_idx = neighbor_idx
             cost_through_best_parent = new_cost_through_neighbor
 
         # Add X_new to tree through best "near" node.
-        n_new = Node(X_new, best_cost_idx, best_cost)
+        n_new = Node(X_new, best_parent_idx, cost_through_best_parent)
+        # print("added node")
+        # print(n_new)
         n_new_idx = tree.add(n_new)
 
         # Connect all neighbors of X_new to X_new if that path cost is less.
         for neighbor_idx in neighbor_idxs:
           # TODO don't search over best_cost_idx (the best parent for n_new)
           n_neighbor = tree.nodes[neighbor_idx]
-          neighbor_cost_through_new = n_new.cost + self.distance(n_neighbor.position,
-                                                                 n_new.position)
+          neighbor_cost_through_new = n_new.cost + X_distance(n_neighbor.position,
+                                                              n_new.position)
           if neighbor_cost_through_new < n_neighbor.cost and not self.has_collision(
                   n_new.position, n_neighbor.position):
             # Best path for neighbor is now through x_new
             n_neighbor.parent = n_new_idx
             n_neighbor.cost = neighbor_cost_through_new
             # Update the neighbor node in the tree.
-            tree[neighbor_idx] = n_neighbor
+            tree.nodes[neighbor_idx] = n_neighbor
+
+        # If the new node is at the goal, so noted.
+        if self.at_goal(n_new.position):
+          tree.goal_node_idxs.append(n_new_idx)
+
+        # Plot
+        ax.scatter(X_new[0], X_new[1], c='black')
+        ax.annotate(str(n_new_idx), (X_new[0] + 0.1, X_new[1] + 0.1), c='blue')
+        X_parent = tree.nodes[n_new.parent].position
+        ax.plot(np.array([X_parent[0], X_new[0]]), np.array([X_parent[1], X_new[1]]), 'black')
 
     if tree.goal_node_idxs:
       print("reached goal at nodes:", tree.goal_node_idxs)
@@ -200,8 +236,12 @@ class World:
       while parent:
         goal_path.append(parent)
         parent = tree.nodes[parent].parent
+      # Print starting at root.
+      goal_path = list(reversed(goal_path))
       print(f"path through {goal_node_idx}: {goal_path}")
       print("cost:", tree.nodes[goal_node_idx].cost)
+    plt.show()
+
 
 w = World()
 w.rrt_star()
